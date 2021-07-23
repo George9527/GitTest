c3638df0486e6877bb37a9aaedf8dd89ef060e85
George9527 <81810104+George9527@users.noreply.github.com>
2021-07-23 20:28:15 +0800

    Update HelloWorld.java

--- 0_HelloWorld/src/HelloWorld.java
+++ 0_HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World！！");
         System.out.println("第一次修改");
+        System.out.println("第二次修改 --- 来自Github的爱");
     }
 }
