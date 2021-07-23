6ae3ffcf9f46b0acdc53c963533df4d9128175d4
ZYF <buubeebuu@126.com>
2021-07-23 13:02:21 +0000

    update 0_HelloWorld/src/HelloWorld.java.

--- 0_HelloWorld/src/HelloWorld.java
+++ 0_HelloWorld/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World！！");
         System.out.println("第一次修改");
         System.out.println("第二次修改 --- 来自Github的爱");
+        System.out.println("第三次修改 --- 来自Gitee的爱");
     }
 }
